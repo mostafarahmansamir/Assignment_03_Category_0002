a8f94e04711420c47c002b7f8541b05bd36f0276
mostafarahmansamir <mostafarahmansamir@gmail.com>
2025-01-31 07:54:45 -0800

    added the header and uncomplete footer section

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+      content: ["./src/**/*.{html,js}"],
+      theme: {
+        extend: {
+          fontFamily: {
+              Sora: ['"Sora"', 'serif'],
+            },
+        },
+  
+      },
+      plugins: [],
+    }
